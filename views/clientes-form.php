3f8c9fb1e93ec955b1f1b596f16c0394933109bb
Matheus Poppl <poppl23@gmail.com>
2019-02-04 16:52:00 -0200

    revisao de adicionar pc atamveer

--- views/clientes-form.php
+++ views/clientes-form.php
@@ -10,15 +10,16 @@
             <i class="fas fa-chevron-left"></i>
         </a>
     <?php endif ?>
-    <h1 class="display-4 m-0"><?php echo $viewInfo["title"] ?> Cliente</h1>
+    <h1 class="display-4 m-0 text-capitalize font-weight-bold"><?php echo $viewInfo["title"]." ".ucfirst($modulo); ?></h1>
 </header>
 <?php $table = false ?>
 <section class="mb-5">
     <form method="POST" class="needs-validation" novalidate>
         <div class="row">
             <?php foreach ($colunas as $key => $value): ?>
-                <?php if(is_array($value["Comment"]) && (!array_key_exists("form", $value["Comment"]) || (array_key_exists("form", $value["Comment"]) && $value["Comment"]["form"] != "false"))) : ?>
-                    <?php if(is_array($value["Comment"]) && (array_key_exists("type", $value["Comment"]) && $value["Comment"]["type"] == "table")): ?>
+                <?php if(isset($value["Comment"]) && array_key_exists("form", $value["Comment"]) && $value["Comment"]["form"] != "false") : ?>
+                    <!-- TIPO TABELA AUXILIAR -->
+                    <?php if(array_key_exists("type", $value["Comment"]) && $value["Comment"]["type"] == "table"): ?> 
                         <?php $table = true ?>
                         <input 
                             type="hidden" 
@@ -26,69 +27,75 @@
                             value="<?php echo isset($dados) && !empty($dados) ? $dados[$value["Field"]] : "" ?>"
                             <?php echo $value["Null"] == "NO" ? "required" : "" ?>
                         />
+                    
                     <?php else: ?>
                         <div class="col-lg-<?php echo isset($value["Comment"]["column"]) ? $value["Comment"]["column"] : "12" ?>">
                             <div class="form-group">
-                                <!-- Label Geral -->
-                                <label class="<?php echo $value["Null"] == "NO" ? "font-weight-bold" : "" ?>" for="<?php echo $value['Field'] ?>">
+                                <!-- Label Geral DE QUALQUER CAMPO DO FORM-->
+                                <label class="<?php echo $value["Null"] == "NO" ? "font-weight-bold" : "" ?>" for="<?php echo lcfirst($value["Field"]) ?>">
                                     <!-- Asterisco de campo obrigatorio -->
                                     <?php if ($value["Null"] == "NO"): ?>
                                         <i class="font-weight-bold" data-toggle="tooltip" data-placement="top" title="Campo Obrigatório">*</i>
                                     <?php endif ?>
                                     <span><?php echo array_key_exists("label", $value["Comment"]) ? $value["Comment"]["label"] : ucwords(str_replace("_", " ", $value['Field'])) ?></span>
                                 </label>
+
                                 <?php if(array_key_exists("type", $value["Comment"]) && $value["Comment"]["type"] == "relacional"): ?>
-                                    <!-- Campos relacionais -->
-                                    <select id="<?php echo 'itxt'.($i);?>" 
-                                            name="<?php echo lcfirst($value['nomecol']);?>"
+                                    <!-- Campos relacionais precisa ser revisado para adicionar e editar-->
+                                    <select id="<?php echo lcfirst($value['Field']);?>" 
+                                            name="<?php echo lcfirst($value['Field']);?>"
                                             class="form-control"
                                             data-anterior="<?php echo isset($dados) ? $dados[$value["Field"]] : "" ?>"
                                             <?php echo $value['Null'] == "NO" ? "required" : "" ?>
                                             >
                                             <option value="" selected >Selecione</option>
-                                        <?php for($j = 0; $j < count($value['relacional']); $j++):?>
-                                            <option value="<?php echo $value['relacional'][$j]['nome'];?>" ><?php echo $value['relacional'][$j]['nome'];?></option>
-                                        <?php endfor;?>     
+                                            <?php for($j = 0; $j < count($value['relacional']); $j++):?>
+                                                <option value="<?php echo $value['relacional'][$j]['nome'];?>" ><?php echo $value['relacional'][$j]['nome'];?></option>
+                                            <?php endfor;?>     
                                     </select>
+
                                 <?php elseif(array_key_exists("type", $value["Comment"]) && $value["Comment"]["type"] == "textarea"): ?>
-                                    <!-- Campo para textos -->
+                                    <!-- Campo para textos longos -->
                                     <textarea
                                         class="form-control" 
                                         name="<?php echo lcfirst($value['Field']);?>" 
                                         data-anterior="<?php echo isset($dados) ? $dados[$value["Field"]] : "" ?>"
-                                        id="<?php echo $value['Field'] ?>"
+                                        id="<?php echo lcfirst($value['Field']);?>"
                                         <?php echo $value['Null'] == "NO" ? "required" : "" ?>
                                     ><?php echo isset($dados) && !empty($dados) ? $dados[$value["Field"]] : "" ?></textarea>
+
                                 <?php elseif(array_key_exists("type", $value["Comment"]) && $value["Comment"]["type"] == "radio"): ?>
                                     <!-- Campo tipo radio -->
                                     <div>
                                         <?php $indexRadio = 0 ?>
-                                        <?php foreach ($value["Comment"]["options"] as $valueRadio => $label): ?>
-                                            <?php $checkedRadio =  $indexRadio == 0 ? "checked" : "" ?>
+                                        <?php foreach ($value["Comment"]["options"] as $valueRadio => $labelRadio): ?>
+                                            <?php $checkedRadio = $indexRadio == 0 ? "checked" : "" ?>
                                             <div class="custom-control custom-radio custom-control-inline">
                                                 <input 
                                                     type="radio" 
                                                     id="<?php echo $valueRadio ?>" 
                                                     value="<?php echo $valueRadio ?>" 
-                                                    name="<?php echo $value["Field"] ?>" 
+                                                    name="<?php echo lcfirst($value["Field"]) ?>" 
                                                     class="custom-control-input" 
                                                     <?php echo $checkedRadio ?>
                                                     <?php echo $value['Null'] == "NO" ? "required" : "" ?>
                                                 >
-                                                <label class="custom-control-label" for="<?php echo $valueRadio ?>"><?php echo $label ?></label>
+                                                <label class="custom-control-label" for="<?php echo $valueRadio ?>"><?php echo $labelRadio ?></label>
                                             </div>
                                             <?php $indexRadio++ ?>
                                         <?php endforeach ?>
                                     </div>
+
+
                                 <?php else: ?>
                                     <!-- Campos de texto normal -->
                                     <input 
                                         type="text" 
                                         class="form-control" 
-                                        name="<?php echo $value['Field'] ?>" 
+                                        name="<?php echo lcfirst($value["Field"]) ?>" 
                                         value="<?php echo isset($dados) && !empty($dados) ? $dados[$value["Field"]] : "" ?>"
                                         data-unico="<?php echo array_key_exists("Key", $value) && $value["Key"] == "UNI" ? "unico" : "" ?>"
-                                        id="<?php echo $value['Field'] ?>"
+                                        id="<?php echo lcfirst($value["Field"]) ?>"
                                         <?php echo $value['Null'] == "NO" ? "required" : "" ?>
                                     />
                                 <?php endif ?>
